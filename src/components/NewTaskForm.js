f4ee9971f82ccbe009a0a7f5218a88f0dd63f736
kgrace61 <110522366+kgrace61@users.noreply.github.com>
2024-03-16 21:52:53 -0500

    omg, hated this. completed lab

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [details, setDetails] = useState("")
+  const [category, setCategory] = useState("")
+
+  
+    function handleDetails(event) {
+      setDetails(event.target.value)
+    }
+    function handleChangeCategory(event){
+      setCategory(event.target.value)
+    }
+    const handleSubmit= (event) => {
+      event.preventDefault()
+      const newTask = {
+        text: details,
+        category: category
+      }
+      onTaskFormSubmit(newTask);
+      setDetails("")
+      setCategory(categories[0])
+    }
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input 
+        type="text" 
+        name="text" 
+        value={details}
+        onChange={handleDetails}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" 
+        value={category}
+        onChange={handleChangeCategory}
+        >
+       {categories.map((category) => category !== "All" ? (
+      <option key={category} value={category}>{category}</option>) : null)}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Task from "./Task.js"
 
-function TaskList() {
+
+function TaskList({ onDelete, tasks }) {
+  const taskElements = tasks.map(task => (
+    <Task key={task.text} task={task} text={task.text} category={task.category} onDelete={onDelete} />
+  ))
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskElements}
     </div>
   );
 }
